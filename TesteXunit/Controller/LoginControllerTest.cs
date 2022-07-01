ff676ea7a3fd52b18a2dc1738b4f7ed774bd106a
Tatiane-Lima <96199481+Tatiane-Lima@users.noreply.github.com>
2022-06-30 21:38:03 -0300

    Er3

---
+++ TesteXunit/Controller/LoginControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Chapter.Controllers;
+using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteXunit.Controller
+{
+    public class LoginControllerTest
+    {
+        [Fact]
+        public void Retornar_Usuario_Invalido()
+        {
+            var fakerepository = new Mock<IUsuarioRepository>();
+            fakerepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+            LoginVIewModel dadosLogin = new LoginVIewModel();
+
+            dadosLogin.Email = "email@email.com";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakerepository.Object);
+
+            //Act
+            var resultado = controller.Login(dadosLogin);
+
+            //Assert
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+        }
+    }
+    }
+
